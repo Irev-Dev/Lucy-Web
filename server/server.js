5714bf3d93ca42daa0fbe891129b1a6ecfd27eb9
charlie <irevdev@protonmail.ch>
2018-03-22 01:13:32 +1100

    Start implementing flashes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // const passport = require('passport');
 // const promisify = require('es6-promisify');
-// const flash = require('connect-flash');
+const flash = require('connect-flash');
 const expressValidator = require('express-validator');
 // const routes = require('./routes/index');
 // const helpers = require('./helpers');
@@ -33,6 +33,19 @@ app.use(expressValidator());
 
 // populates req.cookies with any cookies that came along with the request
 app.use(cookieParser());
+
+// Sessions allow us to store data on visitors from request to request
+// This keeps users logged in and allows us to send flash messages
+app.use(session({
+  secret: process.env.SECRET,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
+// The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
+app.use(flash());
 
 app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname + '/client/index.html'));
