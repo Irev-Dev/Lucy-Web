8db778d5277703927be6c0da1225cf86cc1ead8e
mrhut10 <mrhut10@protonmail.com>
2018-04-24 03:16:47 +1000

    made cacheResponseSkip Logic

--- server/handlers/cacheHandlers.js
+++ server/handlers/cacheHandlers.js
@@ -1,23 +1,54 @@
 const mcache = require('memory-cache');
 
-exports.responseCache = duration => (req, res, next ) => {
-  const key = `__express__${req.originalURL || req.url}`;
-  const cachedBody = mcache.get(key);
-  if (cachedBody) res.send(cachedBody);
-  else {
-    const refreshResponse = (body) => {
-      mcache.put(key, body, duration); // cache body response
-      res.sendResponse(body); // call orgininal .send
-      // carefull calling property above pre defined
-    };
+function objTravers(obj, k1, ...keys) {
+  let output;
+  if (obj === undefined) {
+    output = undefined;
+  } else {
+    output = (keys.length === 0 ? obj[k1] : objTravers(obj[k1], ...keys))
+  }
+  return output;
+}
 
-    // store current .send method & then override .send method
-    [res.sendResponse, res.send] = [res.send, refreshResponse];
-    next(); // pass on to get uncached response
+exports.propHelpLookup = objTravers;
+
+exports.skipResponseCacheSettings = {
+  rules: [{
+    key: 'res.locals.flashes',
+    assert: (prop) => {
+      return prop !== undefined ? Object.keys(prop).length >= 1 : false;
+    }, // will assert there is flashes
+  }],
+};
+
+const truthy = val => val ? true : false;
+
+exports.skipResponseCache = (settings, details) => {
+  return settings.rules.map(rule => rule.assert(objTravers(details, ...rule.key.split('.')))).some(truthy);
+};
+
+/*
+exports.responseCache = duration => (req, res, next) => {
+  if (this.skipResponseCache(this.skipResponseCacheSettings, {req, res})) {
+    next();
+  } else {
+    const key = `__express__${req.originalURL || req.url}`;
+    const cachedBody = mcache.get(key);
+    if (cachedBody) res.send(cachedBody);
+    else {
+      const refreshResponse = (body) => {
+        mcache.put(key, body, duration); // cache body response
+        res.sendResponse(body); // call orgininal .send
+        // carefull calling property above pre defined
+      };
+      // store current .send method & then override .send method
+      [res.sendResponse, res.send] = [res.send, refreshResponse];
+      next(); // pass on to get uncached response
+    }
   }
 };
 
-
+*/
 /* notes
 observation on zeit deployment
   responseTimes 100-200ms faster then uncached alternative
